790dc9710ee7fdaddb7b0a9bd4c98e0e74309337
Heba Ahmed <heba.odesk.tasks@gmail.com>
2019-03-05 23:12:53 +0200

    test controller

--- controllers/products.js
+++ controllers/products.js
@@ -10,4 +10,39 @@ async function getProd (req, res) {
         res.send(products);
 }
 
+async function getProdByID (req, res) {     
+    const product = await Product.findById(req.params.id);
+
+    if(!product) return res.status(404).send('Product with given ID is not found');
+    
+    res.send(product);
+}
+
+async function createProd (req, res) {     
+    const {error} = validate(req.body);
+    if (error) return res.status(404).send(error.details[0].message);
+
+    const category = await Category.findById(req.body.CategoryId);
+    if(!category) return res.status(404).send('Invalide Category');
+
+    //create the new Product
+    const newProduct = new Product({ 
+        Pro_Name: req.body.Pro_Name,
+        Category: {
+            _id: category.id,
+            CategoryName: category.CategoryName
+        },
+        numberInStock: req.body.numberInStock,
+        Pro_Description: req.body.Pro_Description,
+        Pro_Price: req.body.Pro_Price,
+        Pro_IMG: req.body.Pro_IMG
+    });
+
+    await newProduct.save();
+     
+    res.send(newProduct);
+}
+
 exports.getProd = getProd;
+exports.getProdByID = getProdByID;
+exports.createProd = createProd;
--- routes/products.js
+++ routes/products.js
@@ -5,46 +5,16 @@ const {Category} = require('../models/categories')
 const mongoose = require('mongoose');
 const authen = require('../middleware/authen');
 
-const { getProd } = require ('../controllers/products')
+const { getProd, getProdByID, createProd } = require ('../controllers/products')
 
 
-router.get('/',
-getProd);
-
-//============================
-router.get('/:id',async (req, res) =>{
-    const product = await Product.findById(req.params.id);
-
-    if(!product) return res.status(404).send('Product with given ID is not found');
-    
-    res.send(product);
-});
+router.get('/', getProd);
 
 //============================
-router.post('/',authen , async (req , res) => {
-    const {error} = validate(req.body);
-    if (error) return res.status(404).send(error.details[0].message);
+router.get('/:id', getProdByID);
 
-    const category = await Category.findById(req.body.CategoryId);
-    if(!category) return res.status(404).send('Invalide Category');
-
-    //create the new Product
-    const newProduct = new Product({ 
-        Pro_Name: req.body.Pro_Name,
-        Category: {
-            _id: category.id,
-            CategoryName: category.CategoryName
-        },
-        numberInStock: req.body.numberInStock,
-        Pro_Description: req.body.Pro_Description,
-        Pro_Price: req.body.Pro_Price,
-        Pro_IMG: req.body.Pro_IMG
-    });
-
-    await newProduct.save();
-     
-    res.send(newProduct);
-});
+//============================
+router.post('/',authen , createProd);
 //===========================================
 
 router.put('/:id',authen , async (req, res) => {
