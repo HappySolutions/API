94e81c52dc25518ab63f45c9564846e09657ec18
Heba Ahmed <heba.odesk.tasks@gmail.com>
2019-03-06 09:35:27 +0200

    test controller

--- controllers/products.js
+++ controllers/products.js
@@ -16,7 +16,7 @@ async function getProdByID (req, res) {
 ///////////////////////////////
 async function getProdByCat (req, res) { 
     var query = { numberInStock : 5 };    
-    const product = await Product.find(req.query);
+    const product = await Product.find(query);
 
     if(!product) return res.status(404).send('Product with given ID is not found');
     
