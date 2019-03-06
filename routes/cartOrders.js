ec3218fc821e167ebed1f330325429009f3f978d
Heba Ahmed <heba.odesk.tasks@gmail.com>
2019-03-06 15:46:21 +0200

    fix bug

--- routes/cartOrders.js
+++ routes/cartOrders.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const Fawn = require('fawn');
 const { getCartOrders, getCartOrderByID, createCartOrder, updateCartOrder,  deleteCartdOrder } = require ('../controllers/cartOrders')
 
-const mongoose = require('mongoose');
-
-Fawn.init(mongoose);
 
 router.get('/', getCartOrders);
 
