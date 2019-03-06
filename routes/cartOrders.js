ef85c5388973645501b9bcd2e7489240e7d7a32f
Heba Ahmed <heba.odesk.tasks@gmail.com>
2019-03-06 21:04:35 +0200

    add get current user api

--- routes/cartOrders.js
+++ routes/cartOrders.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/authen');
+const admin = require('../middleware/admin');
 const { getCartOrders, getCartOrderByID, createCartOrder, updateCartOrder,  deleteCartdOrder } = require ('../controllers/cartOrders')
 
 
@@ -15,6 +17,6 @@ router.post('/', createCartOrder);
 router.put('/:id', updateCartOrder);
 //===============================================
 
-router.delete('/:id', deleteCartdOrder);
+router.delete('/:id',[auth, admin] ,deleteCartdOrder);
 
 module.exports = router;
